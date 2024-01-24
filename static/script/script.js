5aa32624cbc01bbf3991ecb5529d04fb8cd89f4f
Felilpz <luizfelipe.vieira12@gmail.com>
2024-01-23 21:50:15 -0300

    wip

--- static/script/script.js
+++ static/script/script.js
@@ -1,16 +1,27 @@
-let consult = document.querySelector('#consultar')
+let cepInput = document.querySelector('#cep');
 
-consult.addEventListener('click', () => {
-    let cep = document.querySelector('#cep').value;
+cepInput.addEventListener('input', () => {
+    let cep = cepInput.value;
+
+    if (cep.length === 8) {
+        consultarCEP(cep);
+    }
+});
+
+function consultarCEP(cep) {
     let url = `https://viacep.com.br/ws/${cep}/json/`;
 
     fetch(url)
         .then(function (response) {
             response.json().then(function (data) {
-                console.log(data)
+                console.log(data);
 
-                let body = document.querySelector('#resultado')
-                let p = document.createElement('p')
+                let body = document.querySelector('#resultado');
+                let p = document.createElement('p');
+
+                while (body.firstChild) {
+                    body.removeChild(body.firstChild)
+                }
 
                 p.innerHTML = `
                 <p>Bairro: ${data.bairro}</p>
@@ -18,9 +29,11 @@ consult.addEventListener('click', () => {
                 <p>Cep: ${data.cep}</p>
                 <p>Logradouro: ${data.logradouro}</p>
                 <p>UF: ${data.uf}</p>
-                `
+                `;
 
-                body.appendChild(p)
-            })
-        })
-})
+                body.appendChild(p);
+                cepInput.value = ''
+            });
+        });
+
+}
